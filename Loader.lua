c6270255f3a7407309c17783a1f7d98203297409
xShadow1234 <135708281+xShadow1234@users.noreply.github.com>
2024-03-31 19:58:50 +0200

    Removed unnecessary calls

--- Loader.lua
+++ Loader.lua
@@ -8,8 +8,7 @@ local branch = "master"
 
 local function webImport(file)
     local src = loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Ro-script/%s/Scripts/%s.lua"):format(owner, branch, file)))
-    local thread = task.spawn(src)
-    return thread
+    task.spawn(src)
 end
 
 if game.PlaceId == 16510724413 then
