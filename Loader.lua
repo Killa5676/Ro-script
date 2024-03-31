39b4ed590f812b112dbb43aba89801e33f39a59f
xShadow1234 <135708281+xShadow1234@users.noreply.github.com>
2024-03-31 20:09:35 +0200

    added antiafk

--- Loader.lua
+++ Loader.lua
@@ -6,10 +6,18 @@
 local owner = "Killa5676"
 local branch = "master"
 
+local function antiafk()
+    for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
+        v:Disable()
+    end
+end
+
 local function webImport(file)
     local src = loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Ro-script/%s/Scripts/%s.lua"):format(owner, branch, file)))
     task.spawn(src)
 end
+
+task.spawn(antiafk)
 
 if game.PlaceId == 16510724413 then
    webImport("PetCatchers")
