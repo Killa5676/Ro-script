5f1c97f73be984ec9af870e16d43eea4b8dbea74
xShadow1234 <135708281+xShadow1234@users.noreply.github.com>
2024-03-31 14:43:28 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,10 @@
--- loader soons skids
+local owner = "Killa5676"
+local branch = "master"
+
+local function webImport(file)
+    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Ro-script/%s/Scripts/%s.lua"):format(owner, branch, file)), file .. '.lua')()
+end
+
+if game.PlaceId == 16510724413 then
+   webImport("Pet Catchers")
+end
